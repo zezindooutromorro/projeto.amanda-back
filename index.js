9347a3e95a4687c988cd3c5d62b64aeee729842c
zezindooutromorro <125272727+zezindooutromorro@users.noreply.github.com>
2024-06-25 08:08:45 -0300

    df

--- index.js
+++ index.js
@@ -67,4 +67,21 @@ const alfabeto = {
   document.addEventListener('DOMContentLoaded', function () {
     mostrarAlfabeto();
   });
+
+  
+
+  var div = document.getElementById('log');
+var texto = 'Hoje está um lindo dia!';
+
+function escrever(str, el) {
+  var char = str.split('').reverse();
+  var typer = setInterval(function() {
+    if (!char.length) return clearInterval(typer);
+    var next = char.pop();
+    el.innerHTML += next;
+  }, 100);
+}
+
+escrever(texto, div);
+<div id="log"></div>
   
