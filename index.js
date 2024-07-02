cbde24438fef57879eb23e6082c1138746cde4e9
zezindooutromorro <125272727+zezindooutromorro@users.noreply.github.com>
2024-07-02 08:26:31 -0300

    12

--- index.js
+++ index.js
@@ -1,22 +1,3 @@
- 
-  // Função para mostrar o alfabeto
-  function mostrarAlfabeto() {
-    let output = '';
-    for (let letra in alfabeto) {
-      output += letra + ' é de ' + alfabeto[letra] + '<br>';
-    }
-    document.getElementById('alfabeto').innerHTML = output;
-  }
-  
-  // Verificando se o documento HTML foi completamente carregado antes de chamar a função
-  document.addEventListener('DOMContentLoaded', function () {
-    mostrarAlfabeto();
-  });
-  function mostrarLetra(letra) {
-    document.getElementById('letter-display').textContent = letra;
+function mostrarLetra(letra) {
+  document.getElementById('letter-display').textContent = letra;
 }
-
-
-  
-
- 
