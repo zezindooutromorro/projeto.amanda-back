4c4d3e16af4614a029e384b4b8c7e9df7d055c17
zezindooutromorro <125272727+zezindooutromorro@users.noreply.github.com>
2024-08-26 12:31:46 -0300

    1222

--- index.js
+++ index.js
@@ -14,3 +14,43 @@ function mostrarLetra(texto) {
 
 // Verifica se o JavaScript está carregado
 console.log('JavaScript carregado corretamente.');
+
+function mostrarLetra(texto) {
+  var display = document.getElementById('letter-display');
+  var clickSound = document.getElementById('click-sound');
+
+  if (display) {
+      display.innerText = texto;
+  } else {
+      console.error('Elemento "letter-display" não encontrado.');
+  }
+
+  if (clickSound) {
+      clickSound.play();
+  } else {
+      console.error('Elemento "click-sound" não encontrado.');
+  }
+}
+
+console.log('JavaScript carregado corretamente.');
+function iniciarJogo() {
+  var questions = [
+      { letter: 'A', word: '𝓪𝓫𝓪𝓬𝓪𝔁𝓲' },
+      { letter: 'B', word: '𝓑𝓸𝓵𝓪' },
+      // Adicione mais perguntas aqui
+  ];
+
+  var currentQuestion = questions[Math.floor(Math.random() * questions.length)];
+  document.getElementById('question').innerText = `Qual a palavra para a letra ${currentQuestion.letter}?`;
+  
+  var optionsHtml = questions.map(q => `<button onclick="verificarResposta('${q.word}', '${currentQuestion.word}')">${q.word}</button>`).join('');
+  document.getElementById('options').innerHTML = optionsHtml;
+}
+
+function verificarResposta(selectedWord, correctWord) {
+  if (selectedWord === correctWord) {
+      alert('Correto!');
+  } else {
+      alert('Tente novamente!');
+  }
+}
